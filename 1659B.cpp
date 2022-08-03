f6454ce7a25ade05a6db76672e0917d54422b936
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-03 09:50:54 +0800

    Update 1659B.cpp

--- 1659B.cpp
+++ 1659B.cpp
@@ -1,3 +1,25 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+#define ulli unsigned long long int
+#define lli long long int
+int main(){
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k;
+		cin>>n>>k;
+		string f;
+		cin>>f;
+		if(k%2){
+			int i;
+			for(i=0;i<n;i++){
+				if(f[i]=='1')break;
+			}
+		}
+	}
+	return 0;
+}
