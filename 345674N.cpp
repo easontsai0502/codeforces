4daf8de7325480ccb2ccbc96592fd436bed5954d
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-12 16:49:22 +0800

    345674N

---
+++ 345674N.cpp
@@ -0,0 +1,54 @@
+/*
+[Q]https://codeforces.com/group/iTpKFww7Hn/contest/345674/problem/N
+[]
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+#define ulli unsigned long long int
+#define lli long long int
+int pss(int a,int b){/*猜拳答案機*/
+	/*
+	[a,b]
+	1=剪刀
+	2=石頭
+	3=不～
+	[return]
+	0=輸
+	1=贏
+	2=平
+	*/
+
+}
+int main(){
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+    
+	return 0;
+}
+/*
+[I1]
+2 3
+1 2 -1
+1 2 0
+1 2 1
+[O1]
+-1
+[I2]
+6 5
+1 2 1
+2 3 0
+2 4 1
+2 5 1
+6 2 -1
+[O2]
+1
+[I3]
+5 1
+1 2 0
+[O3]
+0
+*/
