fcae3cf22a67a465f47de550c3084eea7fffcaab
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 08:49:55 +0800

    209884B.pp

---
+++ 209884B.cpp
@@ -0,0 +1,22 @@
+/*
+[Q]https://codeforces.com/group/gRkn7bDfsN/contest/209884/problem/B
+*/
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int n;
+int numlist[1001][1001];
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+	cin>>n;
+	
+	return 0;
+}
+/*
+[I1]
+[O1]
+*/
