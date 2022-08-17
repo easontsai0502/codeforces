82a81ca61ebc1579de9713c40c59696c3ef52cf2
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 09:06:29 +0800

    a

--- 209884B.cpp
+++ 209884B.cpp
@@ -6,14 +6,25 @@
 #include<cmath>
 #include<algorithm>
 using namespace std;
+#define lli long long int
+#define ulli unsigned long long int
+#define pii pair<int,int>
 int n;
 int numlist[1001][1001];
+map<pii,pii> come;
+void solve(){
+
+}
 int main(){
     cin.tie(0);
     cout.tie(0);
     ios::sync_with_stdio(false);
 	cin>>n;
-	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>numlist[i][j];
+		}
+	}
 	return 0;
 }
 /*
