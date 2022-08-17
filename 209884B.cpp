1385d714dad5a1e766cdc785c2c66ff9e5c62c36
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 10:56:15 +0800

    a

--- 209884B.cpp
+++ 209884B.cpp
@@ -7,6 +7,7 @@
 #include<cmath>
 #include<algorithm>
 #include<deque>
+#include<map>
 using namespace std;
 #define lli long long int
 #define ulli unsigned long long int
@@ -16,41 +17,43 @@ int numlist[1001][1001];
 map<pii,pii> come;
 deque<pii> worklist;
 void solve(int x=0,int y=0){
-	if(!come.count({x,y})){
+	if(true){
 		int backx=come[{x,y}].first;
 		int backy=come[{x,y}].second;
 		int nownum=numlist[x][y];
 		int backnum=numlist[backx][backy];
-		if(x || y){
+		if((x || y) && !come.count({x,y})){
 			bool bs;	/*bs=true:下一個數值要比較小，反之*/
 			if(backnum>nownum){
 				bs=false;
 			}else{
 				bs=true;
 			}
-			if(x-1>=0){
-				if(numlist[x-1][y]>nownum ^^ bs){
+			if(x-1>=0 && x-1!=backx){
+				if(numlist[x-1][y]>nownum ^ bs){
 					worklist.push_back({x-1,y});
 				}
 			}
-			if(y-1>=0){
-				if(numlist[x-1][y]>nownum ^^ bs){
+			if(y-1>=0 && y-1!=backy){
+				if(numlist[x-1][y]>nownum ^ bs){
 					worklist.push_back({x,y-1});
 				}
 			}
-			if(x+1<n){
-				if(numlist[x-1][y]>nownum ^^ bs){
+			if(x+1<n && x+1!=backx){
+				if(numlist[x-1][y]>nownum ^ bs){
 					worklist.push_back({x+1,y});
 				}
 			}
-			if(y+1<n){
-				if(numlist[x-1][y]>nownum ^^ bs){
+			if(y+1<n && y+1!=backy){
+				if(numlist[x-1][y]>nownum ^ bs){
 					worklist.push_back({x,y+1});
 				}
 			}
 		}else{
 			worklist.push_back({x+1,y});
 			worklist.push_back({x,y+1});
+			come[{x+1,y}]={0,0};
+			come[{x,y+1}]={0,0};
 		}
 	}
 }
@@ -69,8 +72,8 @@ int main(){
 	solve(0,0);
 	for(int i=0;i<n*n;i++){
 		ans++;
-		int needtodo=worklist.size()
-		for(int i=0;i<needtodo;i++){
+		int needtodo=worklist.size();
+		while(needtodo--){
 			int nowx=(worklist.front()).first;
 			int nowy=(worklist.front()).second;
 			if(nowx==n-1 && nowy==n-1){
