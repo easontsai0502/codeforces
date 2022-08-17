b203fc703fe7caada88d78a6ec1ad95b52ae504d
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 10:12:43 +0800

    a

--- 209884B.cpp
+++ 209884B.cpp
@@ -16,40 +16,42 @@ int numlist[1001][1001];
 map<pii,pii> come;
 deque<pii> worklist;
 void solve(int x=0,int y=0){
-	int backx=come[{x,y}].first;
-	int backy=come[{x,y}].second;
-	int nownum=numlist[x][y];
-	int backnum=numlist[backx][backy];
-	if(x || y){
-		bool bs;	/*bs=true:下一個數值要比較小，反之*/
-		if(backnum>nownum){
-			bs=false;
+	if(!come.count({x,y})){
+		int backx=come[{x,y}].first;
+		int backy=come[{x,y}].second;
+		int nownum=numlist[x][y];
+		int backnum=numlist[backx][backy];
+		if(x || y){
+			bool bs;	/*bs=true:下一個數值要比較小，反之*/
+			if(backnum>nownum){
+				bs=false;
+			}else{
+				bs=true;
+			}
+			if(x-1>=0){
+				if(numlist[x-1][y]>nownum ^^ bs){
+					worklist.push_back({x-1,y});
+				}
+			}
+			if(y-1>=0){
+				if(numlist[x-1][y]>nownum ^^ bs){
+					worklist.push_back({x,y-1});
+				}
+			}
+			if(x+1<n){
+				if(numlist[x-1][y]>nownum ^^ bs){
+					worklist.push_back({x+1,y});
+				}
+			}
+			if(y+1<n){
+				if(numlist[x-1][y]>nownum ^^ bs){
+					worklist.push_back({x,y+1});
+				}
+			}
 		}else{
-			bs=true;
+			worklist.push_back({x+1,y});
+			worklist.push_back({x,y+1});
 		}
-		if(x-1>=0){
-			if(numlist[x-1][y]>nownum ^^ bs){
-				worklist.push_back({x-1,y});
-			}
-		}
-		if(y-1>=0){
-			if(numlist[x-1][y]>nownum ^^ bs){
-				worklist.push_back({x,y-1});
-			}
-		}
-		if(x+1<n){
-			if(numlist[x-1][y]>nownum ^^ bs){
-				worklist.push_back({x+1,y});
-			}
-		}
-		if(y+1<n){
-			if(numlist[x-1][y]>nownum ^^ bs){
-				worklist.push_back({x,y+1});
-			}
-		}
-	}else{
-		worklist.push_back({x+1,y});
-		worklist.push_back({x,y+1});
 	}
 }
 int main(){
