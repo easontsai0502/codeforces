4a90a3fe2d4af4c4d301bdc4cfe28fd16b93d5be
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-19 16:54:58 +0800

    369768J.cpp create

---
+++ 369768J.cpp
@@ -0,0 +1,49 @@
+/*
+[Q]https://codeforces.com/group/m7tZBvb5sH/contest/369768/problem/J
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+#define pii pair<int,int>
+#define plli pair<lli,lli>
+#define pulli pair<ulli,ulli>
+#define INT int
+#define UINT unsigned INT
+
+/*struct*/
+
+/*num*/
+
+/*fn*/
+
+/*main*/
+int main(){
+    return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
