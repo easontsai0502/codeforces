f500d1d5e81c28370afe24311701747d49e5fc09
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-19 08:34:52 +0800

    209884B_2.cpp

---
+++ 209884B_2.cpp
@@ -0,0 +1,44 @@
+/*
+[Q]https://codeforces.com/group/gRkn7bDfsN/contest/209884/problem/B
+[TLE 8]
+[sample ans]http://dreamoon4.blogspot.com/2016/11/13.html
+*/
+
+/*include*/
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<deque>
+#include<map>
+
+using namespace std;
+
+/*define*/
+#define lli long long int
+#define ulli unsigned long long int
+#define pii pair<int,int>
+
+/*fn*/
+
+
+int main(){
+	
+	return 0;
+}
+
+/*
+[I1]
+2
+3 4
+7 5
+[O1]
+3
+[I2]
+3
+15 16 18
+13 11 9
+2 7 3
+[O2]
+7
+*/
