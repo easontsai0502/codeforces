aef0b8d2d5382391b576b3113edd6c6d545503fa
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-22 15:57:49 +0800

    343897F.cpp

---
+++ 343897F.cpp
@@ -0,0 +1,59 @@
+/*
+[Q]https://codeforces.com/group/iTpKFww7Hn/contest/343897/problem/F
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+#define pii pair<int,int>
+#define plli pair<lli,lli>
+#define pulli pair<ulli,ulli>
+#define INT lli
+#define UINT unsigned INT
+
+/*struct*/
+
+/*num*/
+const UINT nmax=1000000;
+UINT n,k;
+UINT a[nmax+1];
+
+/*fn*/
+
+/*main*/
+int main(){
+    cin.tie(0);
+    cout.tie
+    cin>>n>>k;
+    for(UINT i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
