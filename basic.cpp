63cf21e5577b0df9abc4396a6024dd53a5e05e9f
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-25 15:24:57 +0800

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -35,10 +35,10 @@ using namespace std;
 
 /*main*/
 int main(){
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(false)
-    return 0;
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+	return 0;
 }
 
 /*
