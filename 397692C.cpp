c40531aba96dab581e2d7e6f7adafa977cb5e279
ET01 <EasonTsai0502@gmail.com>
2022-09-19 23:00:01 +0800

    WA

--- 397692C.cpp
+++ 397692C.cpp
@@ -1,5 +1,5 @@
 /*
-[Q]https://codeforces.com/gym/397692/problem/C
+[Q]
 []
 */
 
@@ -34,9 +34,22 @@ using namespace std;
 /*fn宣告*/
 
 /*num*/
-
+const INT maxn=1e6;
+INT n,q;
+string s;
+string t[maxn+1];
+INT sss[maxn+1][2];
+INT p[maxn+1];
+INT ans[maxn+1];
+bool wasdo[maxn+1];
 /*fn定義*/
-
+INT sto01(char c){
+	if(c=='0'){
+		return 0;
+	}else{
+		return 1;
+	}
+}
 /*main*/
 int main(){
 	{/*IO加速*/
@@ -45,10 +58,65 @@ int main(){
 		ios::sync_with_stdio(false);
 	}
 	{/*CIN*/
-
+		cin>>n>>q;
+		cin>>s;
+		
+		for(int i=0;i<q;i++){
+			cin>>t[i];
+		}
+		
 	}
 	{/*solve*/
-
+		INT r0=n;
+		INT r1=n;
+		for(INT i=n-1;i>=0;i--){
+			if(s[i]=='0'){
+				r0=i;
+			}else if(s[i]=='1'){
+				r1=i;
+			}
+			sss[i][0]=r0;
+			sss[i][1]=r1;
+		}
+		for(int i=0;i<2;i++){
+			for(int j=0;j<n;j++){
+				cout<<sss[j][i];
+			}
+			cout<<"\n";
+		}
+		for(INT i=0;i<n;i++){
+			cout<<i<<","<<sss[i][0]<<" "<<sss[i][1]<<":";
+			for(int j=0;j<q;j++){
+				if(p[j]==t[j].size()){
+					continue;
+				}
+				INT nowplace=p[j];
+				INT nownum;
+				if(t[j][nowplace]=='1'){
+					nownum=1;
+				}else{
+					nownum=0;
+				}
+				cout<<nownum;
+				if(p[j]<t[j].size() && sss[i][nownum]==i){
+					p[j]++;
+					cout<<"V";
+					wasdo[j]=true;
+				}
+				if(wasdo[j] && p[j]<=t[j].size()){
+					ans[j]++;
+				}
+				cout<<" ";
+			}
+			cout<<"\n";
+		}
+	}
+	for(int i=0;i<q;i++){
+		if(t[i].size()==p[i]){
+			cout<<ans[i]<<"\n";
+		}else{
+			cout<<"-1\n";
+		}
 	}
 	return 0;
 }
@@ -56,6 +124,11 @@ int main(){
 /*
 [I1]
 [O1]
+*/
+
+/*think*/
+/*
+
 */
 
 /*think*/
