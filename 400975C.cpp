56d8e56cde3696af187337d148e55479dc5527b6
ET01 <EasonTsai0502@gmail.com>
2022-10-07 19:30:07 +0800

    100%

--- 400975C.cpp
+++ 400975C.cpp
@@ -1,6 +1,6 @@
 /*
-[Q]
-[]
+[Q]https://codeforces.com/group/VEc3guEDmO/contest/400975/problem/C
+[100%]
 */
 
 /*include*/
@@ -24,8 +24,8 @@ using namespace std;
 /*define type*/
 #define ULLI unsigned long long int
 #define LLI long long int
-#define INT int
-#define UINT unsigned INT
+#define INT ULLI
+#define UINT INT
 #define PII pair<INT,INT>
 #define PUIUI pair<UINT,UINT>
 
@@ -53,10 +53,10 @@ int main(){
 	road.push_back(orig);
 	INT a[n+1],b[n+1];
 	for(INT i=1;i<=n;i++){
-		cin>>a[i];
+		cin>>b[i];
 	}
 	for(INT i=1;i<=n;i++){
-		cin>>b[i];
+		cin>>a[i];
 	}
 	for(INT i=1;i<=n;i++){
 		orig.first=a[i];
@@ -66,18 +66,32 @@ int main(){
 	sort(road.begin(),road.end());
 	INT roadlong[n+1];
 	INT getcandy[n+1];
+	roadlong[0]=getcandy[0]=0;
 	for(INT i=1;i<=n;i++){
-		roadlong[i]=roadlong[i-1]+road[i].first;
+		roadlong[i]=road[i].first;
 		getcandy[i]=getcandy[i-1]+road[i].second;
 	}
+	/*debug*/
+	/*
+	for(INT i=1;i<=n;i++){
+		cout<<roadlong[i]<<" ";
+	}
+	cout<<"\n";
+	for(INT i=1;i<=n;i++){
+		cout<<getcandy[i]<<" ";
+	}
+	cout<<" ";
+	*/
+	/*debug*/
 	while(q--){
 		INT x;
 		cin>>x;
 		INT i=0;
 		INT ans=0;
 		for(INT j=1;j<=n;j++){
-			while(i<j && roadlong[j]-roadlong[i]<=x)i++;
-			ans=max(ans,getcandy[j]-getcandy[i]);
+			while(i<j && (roadlong[j]-roadlong[i])>x)i++;
+			ans=max(ans,getcandy[j]-getcandy[i]+road[i].second);
+			//*debug*/cout<<i<<","<<j<<","<<ans<<" ";
 		}
 		cout<<ans<<"\n";
 	}
