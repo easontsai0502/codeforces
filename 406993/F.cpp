4346c069664e09e5821e107ef84c4407ac699fe7
ET01 <EasonTsai0502@gmail.com>
2022-11-02 15:20:04 +0800

    nothing

---
+++ 406993/F.cpp
@@ -0,0 +1,76 @@
+/*
+[zj]		[Q]https://zerojudge.tw/ShowProblem?problemid= [題目編號]
+[tioj]	[Q]https://tioj.ck.tp.edu.tw/problems/ [題目編號]
+[cses]	[Q]https://cses.fi/problemset/task/ [題目編號]
+[cf]		[Q]https://codeforces.com/group/VEc3guEDmO/contest/406993/problem/F
+[]
+*/
+
+/*include*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O2")
+#pragma GCC optimize("O1")
+#pragma GCC optimize("O3")
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define what_the_fuck cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
+#define ULLI unsigned long long int
+#define LLI long long int
+#define INT int
+#define UINT unsigned INT
+#define PII pair<INT,INT>
+#define PUIUI pair<UINT,UINT>
+#define endl "\n"
+#define wassomething() empty()==false
+
+/*struct*/
+struct super_pair{
+	INT x1=0;
+	INT x2=0;
+	INT x3=0;
+	INT x4=0;
+	INT x5=0;
+};
+/*fn宣告*/
+/*num*/
+bool debug=false;
+bool iofast=true;
+INT n,k,x,y,z;
+const INT mxn=1e5;
+INT a[mxn+1],b[mxn+1],c[mxn+1];
+/*fn定義*/
+/*main*/
+int main(){
+	/*IO加速*/
+	if(!debug&&iofast)what_the_fuck;
+	/*CIN*/
+	cin>>n>>k>>x>>y>>z;
+	for(INT i=0;i<n;i++){
+		cin>>a[i]>>b[i]>>c[i];
+	}
+	/*solve*/
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
