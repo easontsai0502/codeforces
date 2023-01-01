a06ba3ee2e2299cc145abda75cbc82f0bbbdc480
ET01 <66681962+easontsai0502@users.noreply.github.com>
2023-01-01 21:55:11 +0800

    a

---
+++ 417347/pF.cpp
@@ -0,0 +1,89 @@
+/*
+[tioj]			[Q]https://tioj.ck.tp.edu.tw/problems/ [ID]
+[zj]				[Q]https://zerojudge.tw/ShowProblem?problemid= [ID]
+[cses]			[Q]https://cses.fi/problemset/task/ [ID]
+[AtCoder]		[Q]https://atcoder.jp/contests/ [ID] /tasks/ [ID] _ [PID]
+[]
+*/
+
+/*include*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O2")
+#pragma GCC optimize("O1")
+#pragma GCC optimize("O3")
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+#include<cstring>
+#include<iomanip>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define what_the_fuck cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
+#define ULLI unsigned long long int
+#define LLI long long int
+#define INT int
+#define UINT unsigned INT
+#define PII pair<INT,INT>
+#define PUIUI pair<UINT,UINT>
+#define endl "\n"
+#define DBG if(debug)
+#define FIR first
+#define SEC second
+#define elif else if
+#define wassomething() empty()==false
+#define REre return re
+#define P(n,m) pair<n,m>
+#define read(n) reader<n>()
+/*struct宣告*/
+/*fn宣告*/
+/*num*/
+bool debug=0;
+bool iofast=true;
+
+PII mv[]={{0,1},{1,0},{0,-1},{-1,0}};
+INT mx[]={0,1,0,-1};
+INT my[]={1,0,-1,0};
+INT mod=988244353;
+/*struct定義*/
+/*fn定義*/
+
+template<typename TPE>TPE reader(){
+	TPE a;
+	getline(cin,a);
+	return a;
+}
+
+/*main*/
+int main(){
+	//if(!debug&&iofast){what_the_fuck;}
+	/*CIN*/
+	//INT n=read(INT);
+	INT s;
+	cin>>s;
+	cout<<s;
+	while(1)
+	/*solve*/
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
